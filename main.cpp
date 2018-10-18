bb0da99e2789dbe621a8ed70f3b14292287f7485
Kyco9 <44262454+Kyco9@users.noreply.github.com>
2018-10-18 19:16:35 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,40 +4,13 @@ using namespace std;
 
 int main()
 {
-    int s;
-    float a,b;
-    cout << "nhap a = ";cin>>a;
 
-    cout << "nhap b = ";cin>>b;
-
-    cout << "\n 1. de tinh tong \n 2. de tinh hieu \n 3. de tinh tich \n 4. de tinh thuong"<<endl;
-
-    cout << "nhap so ban chon ";cin >>s;
-
-    switch (s)
+    int n,i;
+    cout << "Nhap so lan lap "; cin >> n;
+    for (i=1;i<=n;i++)
     {
-        case 1:
-        {
-            cout << "a + b =" << a+b;
-            break;
-        }
-        case 2:
-        {
-            cout << "a - b= " << a-b;
-            break;
-        }
-        case 3:
-        {
-            cout << "a x b = " << a*b;
-            break;
-        }
-        case 4:
-        {
-            cout << "a chia b = " << a/b;
-            break;
-        }
-
-
+        cout << "Day la lan lap thu "<<i<<endl;
     }
+
     return 0;
 }
